cc6fc1911bc09954ab67347c46de7e39b4b737fe
bharatkumar <kumarbharat878@gmail.com>
2023-06-18 03:54:58 +0530

    implemented Qualifier annotation

--- app/src/main/java/com/bharat/dependencyinjection/NotificationServiceModule.kt
+++ app/src/main/java/com/bharat/dependencyinjection/NotificationServiceModule.kt
@@ -2,12 +2,20 @@ package com.bharat.dependencyinjection
 
 import dagger.Module
 import dagger.Provides
+import javax.inject.Named
 
 @Module
 class NotificationServiceModule {
 
+   @MessageQualifier
     @Provides
     fun getMessageService(): NotificationService {
         return NotificationService.MessageService()
+    }
+
+    @Named("email")
+    @Provides
+    fun getEmailService(): NotificationService {
+        return NotificationService.EmailService()
     }
 }
